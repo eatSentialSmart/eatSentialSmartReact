012d0092ac713cdd25a286c9d2c1bee32359fd74
Ren <rjingmembers@gmail.com>
2018-02-28 21:03:20 -0800

    added a view folder to host page files

---
+++ src/Views/Video.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SearchBar from '../Components/search_bar';
+
+class Video extends Component {
+
+    render() {
+        return (
+            <SearchBar />
+        )
+    }
+}
+
+export default Video; 
