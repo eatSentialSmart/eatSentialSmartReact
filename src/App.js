8ae30037040e2d72d1c413b68df39ff60936683a
Ren <rjingmembers@gmail.com>
2018-03-14 15:27:48 -0700

    created Article page template and added links on navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Video from './Views/Video';
 import Home from './Views/Home';
 import Footer from './Components/footer';
+import Article from './Views/Article';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <Layout />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/articles" component={Article} />
             <Route exact path="/videos" component={Video} />
             <Route path="/" component={Home} />
           </Switch>
---
+++ src/Views/Article.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+const API_KEY = 'b9f91d369ff59547cd47b931d8cbc56b:0:74623931';
+
+class Article extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            articles: []
+        }
+    }
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default Article; 
