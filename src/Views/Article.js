d0e6000278d7efe1af6bdffa10aa782406dddfb3
Ren <rjingmembers@gmail.com>
2018-03-14 21:57:47 -0700

    created a form component with relevant elements and layouts

--- src/Views/Article.js
+++ src/Views/Article.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
+import axios from 'axios';
+import ASForm from '../Components/article_search_form';
 
 const API_KEY = 'b9f91d369ff59547cd47b931d8cbc56b:0:74623931';
+const queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${API_KEY}&q=`;
 
 class Article extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            articles: []
+            articles: [],
+            searchTerm: {}       
         }
+    }
+
+    componentDidMount() {
+        axios({
+            method: 'get',
+            url: `${queryURL}pizza`
+        }).then( res=> {
+            console.log(res);
+        })
     }
 
     render() {
         return(
-            <div></div>
+            <div>
+                <Container>
+                    <ASForm />
+                </Container>
+            </div>
         )
     }
 }
