f3adba0e27824c2035e8d78698dc562509830123
gitrenn <rjingmembers@gmail.com>
2018-03-21 17:39:00 -0700

    obtained data from the zomato api

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -10,37 +10,84 @@ import axios from 'axios';
 const apiRecKey = "e8b036bdd9a6347775ea2c30690c2d94";
 const apiRecID = "7cd61c30";
 
+//GLOBAL VARIABLES FOR GEOLOCATION
+let lat;
+let long;
+let latitude;
+let longitude;
+let coords;
+let position;
+
 export default class Home extends Component {
 
-    componentDidMount() {
-        //SEARCH INPUT;
-        let searchFood = 'cake';
+    // componentDidMount() {
+    //     //SEARCH INPUT;
+    //     let searchFood = 'cake';
 
-        //INDEX DISPLAY TOTAL = 20;
-        let fromIndex = 0;
-        let toIndex = 20;
+    //     //INDEX DISPLAY TOTAL = 20;
+    //     let fromIndex = 0;
+    //     let toIndex = 20;
 
-        //HEALTH INPUT FUNCTION (VEGATARIAN, VEGAN, OMNIVORE);
-        let health = 'sugar-conscious';
-        console.log(health);
+    //     //HEALTH INPUT FUNCTION (VEGATARIAN, VEGAN, OMNIVORE);
+    //     let health = 'sugar-conscious';
+    //     console.log(health);
 
 
-        //HEALTH DEFICIENCES FUNCTION
-        let diet = 'balanced';
-        console.log(diet);
+    //     //HEALTH DEFICIENCES FUNCTION
+    //     let diet = 'balanced';
+    //     console.log(diet);
 
-        let queryURLRec = `https://api.edamam.com/search?q=${searchFood}&app_id=${apiRecID}&app_key=${apiRecKey}&from=${fromIndex}&to=${toIndex}&health=${health}&${diet}&callback=food`;
-        axios({
-            url: queryURLRec,
-            method: 'GET',
-            jsonpCallback: "food",
-            dataType: "jsonp",
-        }).then(res => {
-            console.log(res);
-            console.log(res.data);
-        });
+    //     let queryURLRec = `https://api.edamam.com/search?q=${searchFood}&app_id=${apiRecID}&app_key=${apiRecKey}&from=${fromIndex}&to=${toIndex}&health=${health}&${diet}&callback=food`;
+    //     axios({
+    //         url: queryURLRec,
+    //         method: 'GET',
+    //         jsonpCallback: "food",
+    //         dataType: "jsonp",
+    //     }).then(res => {
+    //         console.log(res);
+    //         console.log(res.data);
+    //     });
+    // }
+    searchZomato = () => {
+
+    
+   
+        this.getLocation = () => {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(this.showPosition);
+            }
+        }
+        this.showPosition = (position) => {
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+            console.log(position.coords);
+            console.log(long);
+            console.log(position);
+            
+            let searchFood = 'cake';
+            let health = 'sugar-conscious';
+            let queryURL = `https://developers.zomato.com/api/v2.1/search?q=${searchFood}%20&health=${health}&lat=${lat}&lon=${long}&sort=real_distance&order=asc`;
+
+            axios({
+                method: "GET",
+                headers: {
+                    'X-Zomato-API-Key': 'c7395f5b6224146e27ac3b2feb756dd7'
+                },
+                url: queryURL,
+                dataType: 'json',
+                processData: true,
+            }).then(res => {
+                console.log(res.data);
+            });
+        };
+        this.getLocation();
     }
 
+    componentDidMount(){
+
+    
+       this.searchZomato();
+    }
     render() {
         return (
             <div>
