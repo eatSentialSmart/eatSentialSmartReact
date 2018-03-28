d3e39339f93ba18f38d01f75945979dc20265acf
Ren <rjingmembers@gmail.com>
2018-03-27 23:12:08 -0700

    fixed nested <a> tag in navbar and sidebar

--- src/Views/MobileLayout.js
+++ src/Views/MobileLayout.js
@@ -36,35 +36,31 @@ export default class MobileContainer extends Component {
         return (
             <Responsive maxWidth={768}>
                 <Visibility once={false} onBottomPassed={this.showFixedMenu} onBottomPassedReverse={this.hideFixedMenu}>
-                <Segment inverted color='blue' textAlign='center' vertical>
-                                <Container>
-                                    <Menu
-                                        fixed={fixed ? 'top' : null}
-                                        inverted={!fixed}
-                                        secondary={!fixed}
-                                        size='large'
-                                        style={{ fontSize: '24px', }}
-                                    >
-                                        <Menu.Item onClick={this.handleToggle}>
-                                            <Icon name='sidebar' />
-                                        </Menu.Item>
-                                        <Menu.Item position='right' name='recipes' onClick={this.handleItemClick}>
-                                            <Link to='/'><Image size='mini' src='/assets/img/logo.png' /></Link>
-                                        </Menu.Item>
-                                        <Menu.Item header style={{ marginLeft: '-2em' }} name='recipes' onClick={this.handleItemClick}><Link to='/'>eatSentialSmart</Link></Menu.Item>
-                                    </Menu>
-                                </Container>
-                            </Segment>
+                    <Segment inverted color='blue' textAlign='center' vertical>
+                        <Container>
+                            <Menu
+                                fixed={fixed ? 'top' : null}
+                                inverted={!fixed}
+                                secondary={!fixed}
+                                size='large'
+                                style={{ fontSize: '24px', }}
+                            >
+                                <Menu.Item onClick={this.handleToggle}>
+                                    <Icon name='sidebar' />
+                                </Menu.Item>
+                                <Menu.Item as={Link} to='/' position='right' name='recipes' onClick={this.handleItemClick}>
+                                   <Image size='mini' src='/assets/img/logo.png' />
+                                </Menu.Item>
+                                <Menu.Item as={Link} to='/' header style={{ marginLeft: '-2em' }} name='recipes' onClick={this.handleItemClick}>eatSentialSmart</Menu.Item>
+                            </Menu>
+                        </Container>
+                    </Segment>
                     <Sidebar.Pushable>
-                        <Sidebar as={Menu} animation='overlay' pointing inverted color='blue' vertical visible={sidebarOpened} style={{ fontSize: '24px' }}>
-                            <Link to='/'>
-                                <Menu.Item name='recipes' active={activeItem === 'recipes'} onClick={this.handleItemClick}>Recipes</Menu.Item></Link>
-                            <Link to='/articles'>
-                                <Menu.Item name='articles' active={activeItem === 'articles'} onClick={this.handleItemClick}>Articles</Menu.Item></Link>
-                            <Link to='/videos'>
-                                <Menu.Item name='videos' active={activeItem === 'videos'} onClick={this.handleItemClick}>Videos</Menu.Item></Link>
+                        <Sidebar as={Menu} animation='overlay' pointing inverted color='blue' vertical visible={sidebarOpened} style={{ fontSize: '24px' }}>                   
+                                <Menu.Item as={Link} to='/' name='recipes' active={activeItem === 'recipes'} onClick={this.handleItemClick}>Recipes</Menu.Item>
+                                <Menu.Item as={Link} to='/articles' name='articles' active={activeItem === 'articles'} onClick={this.handleItemClick}>Articles</Menu.Item>
+                                <Menu.Item as={Link} to='/videos' name='videos' active={activeItem === 'videos'} onClick={this.handleItemClick}>Videos</Menu.Item>
                         </Sidebar>
-
                         <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handlePusherClick} style={{ minHeight: '100vh' }}>
                             {children}
                         </Sidebar.Pusher>
