6b30c3d3ed3effe60474fbac9b15563f28be37f1
Hussein Fakhreddine <hfakhreddine86@gmail.com>
2018-03-29 15:19:24 -0700

    fixed link issue

--- src/Views/MobileLayout.js
+++ src/Views/MobileLayout.js
@@ -29,35 +29,37 @@ export default class MobileContainer extends Component {
         const { activeItem } = this.state
 
         return (
-            <Responsive maxWidth={768}>
-                <Segment inverted color='blue' textAlign='center' vertical>
-                    <Container>
-                        <Menu
-                            inverted={!fixed}
-                            secondary={!fixed}
-                            style={{ fontSize: '24px', }}
-                        >
-                            <Menu.Item onClick={this.handleToggle}>
-                                <Icon name='sidebar' />
-                            </Menu.Item>
-                            <Menu.Item position='right' name='recipes' onClick={this.handleItemClick}>
-                                <Link to='/'><Image size='mini' src='/assets/img/logo.png' /></Link>
-                            </Menu.Item>
-                            <Menu.Item header style={{ marginLeft: '-2em' }} name='recipes' onClick={this.handleItemClick}><Link to='/'>eatSentialSmart</Link></Menu.Item>
-                        </Menu>
-                    </Container>
-                </Segment>
-                <Sidebar.Pushable>
-                    <Sidebar as={Menu} animation='overlay' pointing inverted color='blue' vertical visible={sidebarOpened} style={{ fontSize: '24px' }}>
-                        <Menu.Item as={Link} to='/' name='recipes' active={activeItem === 'recipes'} onClick={this.handleItemClick}>Recipes</Menu.Item>
-                        <Menu.Item as={Link} to='/articles' name='articles' active={activeItem === 'articles'} onClick={this.handleItemClick}>Articles</Menu.Item>
-                        <Menu.Item as={Link} to='/videos' name='videos' active={activeItem === 'videos'} onClick={this.handleItemClick}>Videos</Menu.Item>
-                    </Sidebar>
-                    <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handlePusherClick} style={{ minHeight: '100vh' }}>
-                        {children}
-                    </Sidebar.Pusher>
-                </Sidebar.Pushable>
-            </Responsive>
+            <div>
+                <Responsive maxWidth={768}>
+                    <Segment inverted color='blue' textAlign='center' vertical>
+                        <Container>
+                            <Menu
+                                inverted={!fixed}
+                                secondary={!fixed}
+                                style={{ fontSize: '24px', }}
+                            >
+                                <Menu.Item onClick={this.handleToggle}>
+                                    <Icon name='sidebar' />
+                                </Menu.Item>
+                                <Menu.Item as={Link} to='/' position='right' name='recipes' onClick={this.handleItemClick}>
+                                    <Image size='mini' src='/assets/img/logo.png' />
+                                </Menu.Item>
+                                <Menu.Item as={Link} to='/' header style={{ marginLeft: '-2em', fontSize: '24px' }} name='recipes' onClick={this.handleItemClick}>eatSentialSmart</Menu.Item>
+                            </Menu>
+                        </Container>
+                    </Segment>
+                    <Sidebar.Pushable>
+                        <Sidebar as={Menu} animation='overlay' pointing inverted color='blue' vertical visible={sidebarOpened} style={{ fontSize: '24px' }}>
+                            <Menu.Item as={Link} to='/' name='recipes' active={activeItem === 'recipes'} onClick={this.handleItemClick}>Recipes</Menu.Item>
+                            <Menu.Item as={Link} to='/articles' name='articles' active={activeItem === 'articles'} onClick={this.handleItemClick}>Articles</Menu.Item>
+                            <Menu.Item as={Link} to='/videos' name='videos' active={activeItem === 'videos'} onClick={this.handleItemClick}>Videos</Menu.Item>
+                        </Sidebar>
+                        <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handlePusherClick} style={{ minHeight: '100vh' }}>
+                            {children}
+                        </Sidebar.Pusher>
+                    </Sidebar.Pushable>
+                </Responsive>
+            </div>
         )
     }
 }
